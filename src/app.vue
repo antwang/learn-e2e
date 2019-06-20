488454be16fae10fb36bfadcf37e6d812ccdc8a2
wangchao <wangchao@daojia-inc.com>
2019-06-20 19:49:17 +0800

    init e2e

--- src/app.vue
+++ src/app.vue
@@ -1,36 +1,11 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <ul class="country-list">
-      <li class="ico ico-l1"></li>
-      <li class="ico ico-l2"></li>
-      <li class="ico ico-l3"></li>
-      <li class="ico ico-l4"></li>
-    </ul>
     <router-view/>
   </div>
 </template>
 <script>
-export default {
-  async created() {}
-};
+export default {};
 </script>
-<style lang="scss">
-@import "~sprite2.scss";
-$black: black;
-$yellow: yellow;
 
-h2 {
-  font-size: 20px;
-  color: $black;
-  background: $yellow;
-  text-align: center;
-}
-
-.country-list {
-  display: flex;
-  align-content: center;
-  justify-content: center;
-}
-</style>
 
--- src/pages/Hello.vue
+++ src/pages/Hello.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-    <h2>欢迎使用项目模板！</h2>
-    <p>{{msg}}</p>
+    <h2 class="title">欢迎使用项目模板！</h2>
+    <p class="count">{{count}}</p>
+    <button class="add" type="button" @click="add">add</button>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      msg: "hello"
+      count: 0
     };
   },
-  created() {
-    this.msg = "world";
+  methods: {
+    add() {
+      this.count = this.count + 1;
+    }
   }
 };
 </script>
+<style lang="scss">
+h2 {
+  font-size: 20px;
+  color: #000;
+  background: #ff0;
+  text-align: center;
+}
+</style>
